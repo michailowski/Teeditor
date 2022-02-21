1735a82b1f2c05db89cea01cb86afeff3ffcef7d
Stanislaw Michailowski <s.michajlowski@gmail.com>
2022-02-21 19:48:35 +0100

    Fixed the display of boxes in the sidebar.

--- Teeditor/Views/SidebarControl.xaml.cs
+++ Teeditor/Views/SidebarControl.xaml.cs
@@ -94,9 +94,11 @@ namespace Teeditor.Views
             if (items == null)
                 return;
 
-            for (int i = 0; i < items.Count; i++)
+            var activeItems = items.Where(x => x.Visibility == Visibility.Visible).ToList();
+
+            for (int i = 0; i < activeItems.Count; i++)
             {
-                items[i].SetValue(Grid.RowProperty, i);
+                activeItems[i].SetValue(Grid.RowProperty, i);
             }
         }
 
